ec56945c8b0487b3acae536d2d604ca76431182a
albarral <albarral@migtron.com>
2016-12-06 13:47:29 +0100

    amy testing: new project to test remote amy control

---
+++ amyTesting/nbproject/Makefile-Debug.mk
@@ -0,0 +1,104 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/src/amy/test/ArmCommander.o \
+	${OBJECTDIR}/src/amy/test/ArmComs1.o \
+	${OBJECTDIR}/src/amy/test/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-Wl,-rpath,../amy_utils/dist/Debug/GNU-Linux -L../amy_utils/dist/Debug/GNU-Linux -lamy_utils -Wl,-rpath,../amy_coms/dist/Debug/GNU-Linux -L../amy_coms/dist/Debug/GNU-Linux -lamy_coms
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting: ../amy_utils/dist/Debug/GNU-Linux/libamy_utils.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting: ../amy_coms/dist/Debug/GNU-Linux/libamy_coms.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/src/amy/test/ArmCommander.o: src/amy/test/ArmCommander.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/amy/test
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Isrc -I../amy_utils/src -I../amy_coms/src -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/amy/test/ArmCommander.o src/amy/test/ArmCommander.cpp
+
+${OBJECTDIR}/src/amy/test/ArmComs1.o: src/amy/test/ArmComs1.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/amy/test
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Isrc -I../amy_utils/src -I../amy_coms/src -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/amy/test/ArmComs1.o src/amy/test/ArmComs1.cpp
+
+${OBJECTDIR}/src/amy/test/main.o: src/amy/test/main.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/amy/test
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -Isrc -I../amy_utils/src -I../amy_coms/src -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/amy/test/main.o src/amy/test/main.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../amy_utils && ${MAKE}  -f Makefile CONF=Debug
+	cd ../amy_coms && ${MAKE}  -f Makefile CONF=Debug
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amytesting
+
+# Subprojects
+.clean-subprojects:
+	cd ../amy_utils && ${MAKE}  -f Makefile CONF=Debug clean
+	cd ../amy_coms && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
