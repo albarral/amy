289581da4d862a79daabbea37792e62fea533f94
Albarral <albarral@migtron.com>
2017-10-24 20:33:28 +0200

    amy main: logging improved
    Now logging configuration obtained from .amy folder

--- amy_main/src/amy/main/main.cpp
+++ amy_main/src/amy/main/main.cpp
@@ -21,13 +21,17 @@ using namespace amy;
 
 // starts the amy control for the specified robot
 void runAmy(Robot& oRobot);
+// obtains user's home path
+std::string getHomePath();
 
 log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("amy"));
 
 // main program
 int main(int argc, char** argv) 
 {
-    log4cxx::xml::DOMConfigurator::configure("log4cxx_config.xml");
+    std::string home = getHomePath();
+    std::string configFile = home + "/.amy/log4cxx_config_amy.xml";
+    log4cxx::xml::DOMConfigurator::configure(configFile);    
         
     // use UR5 (should be specified by argument)
     std::string targetRobot = SupportedRobots::UR5;
@@ -102,3 +106,16 @@ void runAmy(Robot& oRobot)
     return;
 }
 
+std::string getHomePath()
+{    
+    // obtain value of HOME environment variable
+    char* pVar = getenv("HOME");    
+    if (pVar!=NULL)
+    {
+        // transform it in a string
+        std::string home(pVar);
+        return home;
+    }
+    else
+        return "";
+}
